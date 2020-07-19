0e9834261522eb9cb6be40c7893a6df2a0d15eef
tuanxn <tuan.uci@gmail.com>
2020-07-19 08:43:19 -0700

    Final solution

--- Complex/Complex.cpp
+++ Complex/Complex.cpp
@@ -4,17 +4,16 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <stdio.h>
 #include "Complex.h"
 
 using namespace std;
 
-int main()
-{
-   Complex c = 3.14;
-   Complex d = (3.14);
-   Complex zero = Complex();
-   Complex both = Complex(1.1, 2.2);
-}
+//int main()
+//{
+//   Complex c = Complex(0.71785581797, 0.4131147882);
+//   cout << c.to_string();
+//}
 
 bool Complex::operator==(const Complex& that) const {
    if (this->get_real() == that.get_real() && this->get_imag() == that.get_imag()) {
@@ -23,8 +22,10 @@ bool Complex::operator==(const Complex& that) const {
    return false;
 }
 
-Complex& Complex::operator= (const Complex& rhs) {
-   *this = Complex(rhs.get_real(), rhs.get_imag());
+Complex& Complex::operator=(const Complex& rhs) {
+   this->set_real(rhs.get_real());
+   this->set_imag(rhs.get_imag());
+   return *this;
 }
 
 bool Complex::operator<(const Complex& that) const {
@@ -50,21 +51,46 @@ Complex Complex::operator-(const Complex& that) const {
 
 Complex Complex::operator*(const Complex& that) const {
    Complex temp;
-   temp.set_real((this->get_real() * that.get_real()) - (this->get_imag() - that.get_imag()));
-   temp.set_imag((this->get_real() * that.get_imag()) - (this->get_imag() - that.get_real()));
+   temp.set_real((this->get_real() * that.get_real()) - (this->get_imag() * that.get_imag()));
+   temp.set_imag((this->get_real() * that.get_imag()) + (this->get_imag() * that.get_real()));
    return temp;
 }
 
 Complex Complex::reciprocal() const {
-   Complex temp;
-   temp.set_real(this->get_real() / this->norm_squared());
-   temp.set_imag((-(this->get_imag())) / this->norm_squared());
-   return temp;
+
+   //try {
+
+      if (this->norm_squared() <= Complex::FLOOR)
+         throw Div_By_Zero_Exception();
+
+      Complex temp;
+      temp.set_real(this->get_real() / this->norm_squared());
+      temp.set_imag((-(this->get_imag())) / this->norm_squared());
+      return temp;
+   //}
+   //catch (Complex::Div_By_Zero_Exception e) {
+   //   cerr << e.what() << endl;
+   //   exit(-1);
+   //}
+
 }
 
 Complex Complex::operator/(const Complex& that) const {
-   
-   Complex recip_that = that.reciprocal();
-   Complex temp = *this * recip_that;
-   return temp;
+
+      Complex recip_that = that.reciprocal();
+      Complex temp = *this * recip_that;
+      return temp;
+
+}
+
+string Complex::to_string() const {
+   char buf[50];
+   //sprintf(buf, "%f", _real);
+   snprintf(buf, sizeof(buf), "(%.11g,%.11g)", _real, _imag);
+   return string(buf);
+}
+
+ostream& operator<<(ostream& os, const Complex& x) {
+   os << x.to_string();
+   return os;
 }
